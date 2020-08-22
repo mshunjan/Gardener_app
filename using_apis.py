44ea31e95668807cbe04da3162b9a1727b092400
Brandon Ye <bdye27@gmail.com>
2020-08-21 21:01:02 -0700

    Added Search and Retrieve api calls, and merged them into a single function

--- using_apis.py
+++ using_apis.py
@@ -1,10 +1,37 @@
 import requests  
-import json
+import json 
 
-# user_in = input('name a plant:')
-search_par = {'q': 'eastern teaberry', 'token' : 'e8GOvXk9xSCoNQgjLg95RyxkXa1zpPMxv-T2croQ09w'}
+API_TOKEN = 'e8GOvXk9xSCoNQgjLg95RyxkXa1zpPMxv-T2croQ09w'
 
-plant_search = requests.get("https://trefle.io/api/v1/plants/search", search_par)
+def __getPlantIdByName(name):
+    search_par = {'q': name, 'token' : API_TOKEN}
 
-p_dict = plant_search.json() 
-id = p_dict['data'][0]['id']  
+    plant_search = requests.get("https://trefle.io/api/v1/plants/search", search_par)
+
+    p_dict = plant_search.json() 
+    return p_dict['data'][0]['id']  
+
+
+def __getPlantDetailsById(id):
+    retrieve_par = {'token' : API_TOKEN}
+    plant_retrieve = requests.get("https://trefle.io/api/v1/plants/{}".format(id), retrieve_par)
+
+    plant = plant_retrieve.json()['data']
+
+    return {
+        'id': plant['id'],
+        'common_name': plant['common_name'],
+        'scientific_name': plant['scientific_name'],
+        'isVegetable': plant['vegetable'],
+        'image_url': plant['image_url']
+    }
+
+def getPlantDetailsByName(name):
+    id = __getPlantIdByName(name)
+    details = __getPlantDetailsById(id)
+
+    print(details)
+    return details
+
+
+getPlantDetailsByName("eastern teaberry")
